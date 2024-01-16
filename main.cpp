b4658bf8685f6c52b345760f0669eba40f4c64df
phurich <99466867+phompat@users.noreply.github.com>
2024-01-16 17:22:21 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,77 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct  Node{
-    int value;
-    struct Node* link;
+//polymorphism
+
+class Abstractemployee{
+    virtual void Askforpromotion()=0;
 };
+
+class Employee:Abstractemployee{
+private:
+    string name,company;
+    int age;
+public:
+    void setname(string Name){name=Name;}
+    string getname(){return name;}
+    void setcompany(string Company){company=Company;}
+    string getcompany(){return company;}
+    void setage(int Age){
+        if(Age>=18)age=Age;
+    }
+    int getage(){return age;}
+
+    //class method
+    void introduceyourself(){
+        cout<<"Name"<<" "<<name<<endl<<"Company"<<" "<<company<<endl;
+        cout<<"Age"<<" "<<age<<endl<<"********************"<<endl;
+    }
+    void Askforpromotion(){
+        if(age>30)cout<<name<<" got promoted"<<endl;
+        else cout<<name<<" did not get promoted"<<endl;
+    }
+    virtual void Work(){//**coding virtual allows to make line 72 work.
+        cout<<name<<" is checking email , task backlog, performing tasks ...."<<endl;
+    }
+
+    //constructor
+    Employee(string Name ,string Company , int Age){
+        name=Name,company=Company,age=Age;
+    }
+
+};
+
+
+//inheritance
+class Developer:public Employee{
+public:
+    string favprogramlanguage;
+    Developer(string Name ,string Company , int Age, string Favprogramlanguage)
+        :Employee(Name,Company,Age)
+    {
+        favprogramlanguage=Favprogramlanguage;
+    }
+
+     void fixbug(){
+        cout<<getname()<<" fixed bug using "<<favprogramlanguage<<endl;
+     }
+     void Work(){
+        cout<<getname()<<" is writting "<<favprogramlanguage<<" code"<<endl;
+    }
+};
+
+
+
 int main()
 {
-    Node *p=NULL,*h=NULL ;
-    int menu;
-        while(true){
-            cout<<"(Number 1: Add)"<<endl<<"(Number 2: Delete)"<<endl<<"(Number 3: Edit)"<<endl<<"(Number 4: Search)"<<endl;
-            cout<<"(Number 5: Show)"<<endl<<"(Number 6: Exit)"<<endl;
-            cout<<"Enter menu",cin>>menu;
-            if(menu==1){//add
-                p=(Node *)malloc(sizeof(Node));//find address
-                cout<<"enter value",cin>>p->value;
-                p->link=h,h=p;
-            }
-            else if(menu==2){//**delete
-                int d;
-                cout<<"Delete for :";
-                cin>>d;
-                p=h;
-                while(p!=NULL&&p->value!=d)p=p->link;
-                if(p==NULL)cout<<"No Data"<<endl;
-                else{
-                    if(p==h)h=h->link;
-                    else{
-                        Node *pp;
-                        pp=h;
-                        while(pp->link!=p)pp=pp->link;
-                        pp->link=p->link;
-                    }
-                    free(p);
-                    cout<<"Delete complete"<<endl;
-                }
-            }
-            else if(menu==3){//**edit
-                p=h;
-                int edit;
-                cout<<"Edit to";
-                cin>>edit;
-                while(p!=NULL&&p->value!=edit){
-                    p=p->link;
-                }
-                if(p==NULL)cout<<"No Data"<<endl;
-                else{
-                    cout<<"Change to";
-                    cin>>p->value;
-                    cout<<"Edit Complete"<<endl;
-                }
-            }
-            else if(menu==4){//***search
-                p=h;
-                int want;
-                cout<<"Search value :";
-                cin>>want;
-                while(p!=NULL&&p->value!=want){
-                    p=p->link;
-                }
-                if(p==NULL)cout<<"NO"<<endl;
-                else cout<<"YES"<<endl;
-            }
-            else if(menu==5){//***show
-                p=h;
-                while(p!=NULL){
-                    cout<<p->value<<endl;
-                    p=p->link;
-                }
-            }
-            else if(menu==6){
-                return 0;
-            }
-        }
-    return 0;
+    Developer d=Developer("Rod","Amazon",30,"c++");
+    d.fixbug();
+    d.Askforpromotion();
+    d.Work();
+
+
+    Employee *e=&d;
+    e->Work();
+
 }
